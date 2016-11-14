11efc93c6d0ee147588c1c8952c54da268a1ee64
tim-geier <mail@tim-geier.de>
2016-11-14 17:23:14 +0100

    An neues Konzept angepasst, funktionsfähig
    Code läuft, Refactoring aber dringend nötig...
    Primitive AI ist im Moment noch mit drin.
    Fehlerhafte Eingaben werden aktuell noch ohne Meldung einfach ignoriert.
    Spielfeld wird jetzt dynamisch generiert
    Funktion zum neustarten/beenden

--- userInterface.py
+++ userInterface.py
@@ -1,7 +1,20 @@
+# coding: utf-8
+
 import time
 import os
+import random
+#import ai.py
+#import gameCheck.py
 
-logo = """
+# region "constants / settings"
+
+PLAYER1_SYMBOL = "X"
+PLAYER2_SYMBOL = "O"
+
+GRID_WIDTH = 6
+GRID_HEIGHT = 8
+
+LOGO = """
 ██╗  ██╗     ██████╗ ███████╗██╗    ██╗██╗███╗   ██╗███╗   ██╗████████╗
 ██║  ██║    ██╔════╝ ██╔════╝██║    ██║██║████╗  ██║████╗  ██║╚══██╔══╝
 ███████║    ██║  ███╗█████╗  ██║ █╗ ██║██║██╔██╗ ██║██╔██╗ ██║   ██║   
@@ -11,72 +24,166 @@ logo = """
                                                                                                                       
 """
 
+MENU = """
+Hauptmenü:
 
-data = [[0,0,0,0,0,0], \
-    [0,0,0,0,0,0], \
-    [0,0,0,0,0,0], \
-    [0,0,0,0,0,0], \
-    [0,0,0,0,0,0], \
-    [0,0,0,0,0,0], \
-    [0,0,0,0,0,0], \
-    [0,0,0,0,0,0]]
+    Das Spiel kann jederzeit durch Eingabe von 'exit' beendet werden.
+    Oder mit 'restart' neu gestartet werden
+
+    Einspieler Modus starten:   1
+    Zweispieler Modus starten:  2
+
+    Programm beenden:           exit
+
+"""
+
+#endregion
+
+# region "initialize game_data list"
+game_data = []
+
+def initGame(target_list):
+    global actual_player
+    actual_player = 1
+    if len(target_list) < 1:
+        for i in range(GRID_HEIGHT):
+            temp_list = []
+            for j in range(GRID_WIDTH):
+                temp_list.append(0)
+            target_list.append(temp_list)
+    else:
+        for i in range(GRID_HEIGHT):
+            for j in range(GRID_WIDTH):
+                target_list[i][j] = 0
+
+# endregion
+
+# region "print game_data to console"
 
 def printGame():
-    os.system("cls")
-    print("| 1 | 2 | 3 | 4 | 5 | 6 |")
-    print("-------------------------")
-    for i in range(0,8):
-        for j in range(0,6):
+    clearConsole()
+    game_field = "|"
+    for i in range(GRID_WIDTH):
+        game_field += " " + str(i+1) + " |"
+    game_field += "\n"
+    game_width = len(game_field) - 1
+    for i in range(game_width):
+        game_field += "-"
+    game_field += "\n"
+    for i in range(GRID_HEIGHT):
+        for j in range(GRID_WIDTH):
             if j == 0:
-                print("|", end="")
-            if data[i][j] == 0:
-                print("   |", end="")
-            elif data[i][j] == 1:
-                print(" X |", end="")
-            elif data[i][j] == 2:
-                print(" O |", end="")
-        print()
-    print("-------------------------")
+                game_field += "|"
+            game_field += " "
+            if game_data[i][j] == 0:
+                game_field += " "
+            elif game_data[i][j] == 1:
+                game_field += PLAYER1_SYMBOL
+            elif game_data[i][j] == 2:
+                game_field += PLAYER2_SYMBOL
+            game_field += " |"
+        game_field += "\n"
+    
+    for i in range(game_width):
+        game_field += "-"
+    game_field += "\n"
 
-player = False
+    print(game_field)
 
-def dropCoin(col):
-    for i in range(7,-1,-1):
-        if not data[i][col]:
-            if player:
-                data[i][col] = 1
-                break
-            else:
-                data[i][col] = 2
-                break
+def clearConsole():
+    os.system("cls")
+
+# endregion
+
+# region "dropCoin"
+
+actual_player = 1
+
+def drop_coin(col):
+    global actual_player
+    for i in range(GRID_HEIGHT - 1, -1, -1):
+        if not game_data[i][col]:
+            if actual_player == 1:
+                game_data[i][col] = 1
+                actual_player = 2
+                return True
+            elif actual_player == 2:
+                game_data[i][col] = 2
+                actual_player = 1
+                return True
     else:
-        pass
+        return False
 
-def checkWinner():
-    for i in range(0,len(data)):
-        for j in range(0,len(data[i])):
-            try:
-                if data[i][j] == data[i - 1][j] == data[i -2][j] == data[i][j + 1] and data[i][j] != 0:
-                    print("X" if data[i][j] == 1 else "O", "hat gewonnen")
-                    time.sleep(2)
-            except:
-                pass
+# endregion
 
-print(logo)
-time.sleep(0.5)
+# region "game logic/main menu"
+"""
 
-while 1:
+possible game_state's:
+    0   main menu
+    1   singleplayer, game running
+    2   multiplayer, game running
+"""
 
-    printGame()
-    s = input("Bitte Spalte eingeben: ")
+def getUserInput():
+    global game_state
+    temp_input = input("Bitte Spalten Nr. eingeben: ")
+    if temp_input == "exit":
+        clearConsole()
+        game_state = 0
+        return -1
+    elif temp_input == "restart":
+        initGame(game_data)
+        return -1
     try:
-        s = int(s)
+        temp_input = int(temp_input)
+        if temp_input < 1: temp_input = 1
+        elif temp_input > GRID_WIDTH: temp_input = GRID_WIDTH
+        return temp_input
     except:
         print("Falsche Eingabe")
+        return -1
+
+while 1:
+    clearConsole()
+    print(LOGO)
+    print(MENU)
+
+    a = input("Bitte Auswahl treffen: ")
+    if a == "exit":
         break
-    if s < 1: s=1
-    if s>6:s=6
-    player =  not player
-    dropCoin(int(s)-1)
-    printGame()
-    checkWinner()
+    else:
+        if a.isnumeric():
+            s = int(a)
+            game_state = s
+        else:
+            print("Falsche Eingabe")
+            continue
+
+    initGame(game_data)
+
+    while game_state == 2:
+        printGame()
+        print("Spieler", actual_player, "ist am Zug")
+        s = getUserInput()
+        if s < 0:
+            continue
+        drop_coin(int(s)-1)
+        # TODO: check State
+
+    while game_state == 1:
+        printGame()
+        if actual_player == 1:
+            s = getUserInput()
+            if s < 0:
+                continue
+            if not drop_coin(int(s)-1): continue
+        else:
+            print("Computer denkt...")
+            # TODO: AI Implementieren
+            while not drop_coin(random.randint(0, GRID_WIDTH-1)):
+                pass
+            time.sleep(1)
+        # TODO: check State
+   
+# endregion
