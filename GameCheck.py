4e453cbab874ef1f2b0080ca3f75f95cd6181bc3
Klotz95 <NicoKotlenga@live.de>
2016-11-15 23:47:40 +0100

    Implement check_for_win_of method. Had no time for testing (maybe we could do this on Wednesday?)

--- GameCheck.py
+++ GameCheck.py
@@ -2,9 +2,9 @@
 
 """
 
-__author__ = "123456:6345060 Nico Kotlenga, 6404053: Tim Geier"
+__author__ = "6345060 Nico Kotlenga, 6404053: Tim Geier"
 __copyright__ = "Copyright 2016 – EPR-Goethe-Uni"
-__email__ = "nico.kotlenga@stud.uni frankfurt.de, uni@tim-geier.de"
+__email__ = "nico.kotlenga@stud.uni-frankfurt.de, uni@tim-geier.de"
 
 
 class GameCheck:
@@ -19,9 +19,9 @@ class GameCheck:
 
 
     def is_game_finish(self):
-        """This method checks if the current game is finish and returns a
-           boolean value
-
+        """This method checks if the current game is finish and returns 0
+            if the game is still in progress. Otherwise it will return the
+            number of the player who has won the match
         """
         # cut the game field (this will improve the performance of the check)
 
@@ -35,14 +35,80 @@ class GameCheck:
                     break
 
         if(len(cuttedGameField) >= 2):
-            return __check_for_Win_Of("X" , cuttedGameField) /
-                   or __check_for_Win_Of("O", cuttedGameField)
+            if(__check_for_Win_Of("X", cuttedGameField)):
+                return 1
+            elif(__check_for_Win_Of("0", cuttedGameField)):
+                return 2
 
-        return False
+        return 0
 
-    def __check_for_Win_Of(playerSign, cuttedGameField):
+
+
+
+    def __check_for_Win_Of(self, playerSign, cuttedGameField):
         """This method checks if a specific player has won the game.
            Therefor the method has the parameter playerSign which has to be a
            string and contains the player Symbol like "X" , "O"...
 
         """
+
+        countOfLines = len(cuttedGameField)
+        countOfRows = len(cuttedGameField[0]) # countOfLines >=2 no exception
+
+        for y in range(countOfLines):
+            currentCountOfSignsInARow = 0
+            for x in range(countOfRow):
+                if(cuttedGameField[y][x] == playerSign):
+                     currentCountOfSignsInARow++
+                else:
+                     if(currentCountOfSignsInARow == 2):
+                         # check for two bot and top
+                         if(countOfLines - (y + 1) >= 2):
+                             # prevent index out of range exception
+                             if((cuttedGameField[y + 1][x - 2] == playerSign /
+                             and cuttedGameField[y + 2][x - 2] == playerSign)/
+                             or (cuttedGameField[y + 1][x - 1] == playerSign /
+                             and cuttedGameField[y + 2][x - 1] == playerSign)):
+                                return True
+                         if(y >= 2):
+                             if((cuttedGameField[y - 1][x - 2] == playerSign /
+                             and cuttedGameField[y - 2][x - 2] == playerSign)/
+                             or (cuttedGameField[y - 1][x - 1] == playerSign /
+                             and cuttedGameField[y - 2][x - 1] == playerSign)):
+                                return True
+
+                     elif(currentCountOfSignsInARow == 3):
+                         if(countOfLines - (y + 1) >= 1):
+                             for i in range(3, 0, - 2):
+                                 if(cuttedGameField[y + 1][x - i] == /
+                                 playerSign):
+                                    return True
+                         if(y >= 1):
+                             for i in range(3, 0, - 2):
+                                 if(cuttedGameField[y - 1][x - i] == /
+                                 playerSign):
+                                    return True
+                         if(countOfLines - (y + 1) >= 2 and /
+                         cuttedGameField[y + 1][x - 2] == playerSign and /
+                         cuttedGameField[y + 2][x - 2] == playerSign):
+                            return True
+                         if(y >= 2 and cuttedGameField[y - 1][x - 2] == /
+                         playerSign and cuttedGameField[y - 2][x - 2] == /
+                         playerSign):
+                            return True
+
+                     elif(currentCountOfSignsInARow > 3):
+                         for i in range(currentCountOfSignsInARow, 0 , -1):
+                             if(countOfLines - (y + 1) >= 1):
+                                 if(cuttedGameField[y + 1][x - i] == /
+                                 playerSign):
+                                    return True
+                             if(y >= 1):
+                                 if(cuttedGameField[y - 1][x - i] == /
+                                 playerSign):
+                                    return True
+
+                     # reset currentCountOfSignsInARow
+                     currentCountOfSignsInARow = 0
+        #if no combination has been found 
+        return False
